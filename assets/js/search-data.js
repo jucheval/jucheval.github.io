5c06b36d4dc10f144e5851a4928b70a72a58007b
jucheval <julien.chevallier1@univ-grenoble-alpes.fr>
2025-12-28 08:08:44 +0000

    Deploying to gh-pages from @ jucheval/jucheval.github.io@d7dbafcc97b7f94451f42ae5c6c90b9ea66c7d2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,7 +483,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/TODO.pdf", "_blank");
+          window.open("/assets/pdf/cv_fr.pdf", "_blank");
         },
       },{
         id: 'social-email',
